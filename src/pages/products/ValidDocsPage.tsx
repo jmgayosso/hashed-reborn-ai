99cf122cdea5e68dcb288aee940dd2dc78ad379c
Jose Maria Gayosso <josemaria@hashed.one>
2025-07-03 11:45:22 -0600

    Fixed external link

--- src/pages/products/ValidDocsPage.tsx
+++ src/pages/products/ValidDocsPage.tsx
@@ -4,7 +4,7 @@ export default function ValidocsPage() {
     return (
       <div>
         <iframe
-          src="https://jmgayosso.github.io/generated/ValidocsHTML.html"
+          src="https://jmgayosso.github.io/hashed-reborn-ai/generated/ValidocsHTML.html"
           width="100%"
           height="1000px"
           style={{ border: 'none' }}
