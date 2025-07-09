6b86ea59e6790d8cd9057add4ff0ee39e0152b67
Jose Maria Gayosso <josemaria@hashed.one>
2025-07-09 13:07:27 -0600

    Removed www

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Validocs />} />
           <Route path="/home" element={<Index />} />
@@ -36,7 +36,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/pages/products/ValidDocsPage.tsx
+++ src/pages/products/ValidDocsPage.tsx
@@ -4,7 +4,7 @@ export default function ValidocsPage() {
     return (
       <div>
         <iframe
-          src="https://www.hashed.one/generated/ValidocsHTML.html"
+          src="https://hashed.one/generated/ValidocsHTML.html"
           width="100%"
           height="1000px"
           style={{ border: 'none' }}
