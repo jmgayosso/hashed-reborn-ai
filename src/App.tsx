133f342126bb24c5b45f497a27b7338d45ea0a91
Jose Maria Gayosso <josemaria@hashed.one>
2025-08-28 14:22:54 -0600

    Update routing in App component to prioritize Index page

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Validocs />} />
-          <Route path="/home" element={<Index />} />
+          <Route path="/" element={<Index />} />
+          {/* <Route path="/" element={<Validocs />} /> */}
+          {/* <Route path="/home" element={<Index />} /> */}
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
