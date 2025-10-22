f8a6bc53d60448cb2af7e77d86c384c9ee5aa8e5
Jose Maria Gayosso <josemaria@hashed.one>
2025-10-22 09:30:11 -0600

    Remove commented-out product entry for MASTER TIKTOK in products.ts

--- src/const/products.ts
+++ src/const/products.ts
@@ -8,15 +8,15 @@ const products: ProductItem[] = [
     productSummary: 'Intelligent document collection and validations',
     services: ['AI', 'Automation', 'SAAS'],
     redirectTo: 'validocs'
- },
- {
-    id: 2,
-    name: 'MASTER TIKTOK',
-    image: 'images/products/masterTiktokProd.png',
-    productSummary: 'Track brand presence, analyze competitors, and spot industry trends.',
-    services: ['Analytics', 'Social Media', 'Marketing'],
-    redirectTo: ''
  }
+//  {
+//     id: 2,
+//     name: 'MASTER TIKTOK',
+//     image: 'images/products/masterTiktokProd.png',
+//     productSummary: 'Track brand presence, analyze competitors, and spot industry trends.',
+//     services: ['Analytics', 'Social Media', 'Marketing'],
+//     redirectTo: ''
+//  }
 ]
 
 export default products
